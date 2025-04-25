448193ed9fc60c62c89b7dd0897e9b83cd38f0fd
tanvir <tanvirulislam149@gmail.com>
2025-04-26 00:06:31 +0600

    footer done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 const poppins = Poppins({
   variable: "font-poppins",
@@ -15,7 +17,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="dark">
-      <body className={`${poppins.className}`}>{children}</body>
+      <body className={`${poppins.className}`}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Navbar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner/Banner";
 import About from "../../components/About/About";
 import Plans from "../../components/Plans/Plans";
@@ -8,14 +6,10 @@ import Fitness_classes from "../../components/Fitness_classes/Fitness_classes";
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Banner />
       <About />
       <Plans />
       <Fitness_classes />
-      <h1 className="h-screen">asdf</h1>
-      <h1 className="h-screen">asdf</h1>
-      <h1 className="h-screen">asdf</h1>
     </>
   );
 }
