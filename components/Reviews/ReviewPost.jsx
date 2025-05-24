97d7a0ddb3c6ba262dc2a69ed64b732e8f1ef8de
tanvir <tanvirulislam149@gmail.com>
2025-05-24 23:29:07 +0600

    review done

--- components/Reviews/ReviewPost.jsx
+++ components/Reviews/ReviewPost.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import api_client from "@/api_client";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
-const ReviewPost = () => {
+const ReviewPost = ({ id, fetchReview }) => {
+  const [loading, setLoading] = useState(false);
+  const user = useSelector((state) => state?.user?.user);
+
+  const handleReview = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    api_client
+      .post(`http://127.0.0.1:8000/fitness_classes/${id}/reviews/`, {
+        rating: parseInt(e.target.post_rating.value),
+        comment: e.target.comment.value,
+      })
+      .then((res) => {
+        console.log(res.data);
+        fetchReview();
+        document.getElementById("myForm").reset();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  };
   return (
-    <div className="my-4">
+    <form id="myForm" onSubmit={(e) => handleReview(e)} className="my-4">
       <div>
         <label htmlFor="" className="font-bold text-xl">
           Rating:
         </label>
         <br />
-        <div className="rating">
+        <div className="rating post_review">
           {[1, 2, 3, 4, 5].map((star) => (
             <input
               key={star}
               type="radio"
+              value={star}
               name={`post_rating`}
               className="mask mask-star-2 bg-green-400"
               aria-label="1 star"
@@ -26,14 +48,22 @@ const ReviewPost = () => {
       </label>
       <br />
       <textarea
-        name=""
+        name="comment"
         className="textarea lg:w-1/2 w-full"
         placeholder="Enter your comment"
         id=""
       ></textarea>
       <br />
-      <button className="btn btn-primary mt-2 text-black">Submit</button>
-    </div>
+      {!user && (
+        <p className="text-red-400">Please login first to give review.</p>
+      )}
+      <input
+        type="submit"
+        disabled={loading || !user}
+        value={loading ? "Submitting..." : "Submit"}
+        className="btn btn-primary mt-2 text-black disabled:text-gray-400"
+      />
+    </form>
   );
 };
 
--- components/Reviews/Reviews.jsx
+++ components/Reviews/Reviews.jsx
@@ -10,21 +10,24 @@ const Reviews = ({ id }) => {
   const [loading, setLoading] = useState(false);
 
   console.log(reviews);
-
-  useEffect(() => {
+  const fetchReview = () => {
     setLoading(true);
     axios
       .get(`http://127.0.0.1:8000/fitness_classes/${id}/reviews/`)
       .then((res) => setReviews(res.data))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchReview();
   }, []);
   return (
     <div className="my-20">
       <p className="text-primary font-bold text-3xl lg:text-5xl mb-10">
         -Reviews
       </p>
-      <ReviewPost />
+      <ReviewPost id={id} fetchReview={fetchReview} />
       {loading ? (
         <div className="flex justify-center h-50">
           <span className="loading loading-spinner loading-xl"></span>
@@ -34,35 +37,36 @@ const Reviews = ({ id }) => {
           {reviews?.length ? (
             reviews.map((r) => (
               <div key={r.id} className="h-full bg-white rounded-4xl">
-                <div className="text-black p-10">
-                  <div className="flex justify-center">
-                    <div className="rating">
-                      {[1, 2, 3, 4, 5].map((star) => (
-                        <input
-                          key={star}
-                          type="radio"
-                          name={`rating-${r.id}`}
-                          className="mask mask-star-2 bg-green-500"
-                          aria-label="1 star"
-                          checked={r.rating === star}
-                          readOnly
+                <div className="text-black p-5">
+                  <div className="flex items-center mb-3">
+                    <div className="avatar flex justify-center mr-2">
+                      <div className="w-14 rounded-full">
+                        <img
+                          src={
+                            r.user.image
+                              ? `https://res.cloudinary.com/tanvirulislam149/${r.user.image}`
+                              : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                          }
                         />
-                      ))}
+                      </div>
+                    </div>
+                    <div>
+                      <p className="text-center font-bold">{r.user.email}</p>
+                      <div className="rating review_rating">
+                        {[1, 2, 3, 4, 5].map((star) => (
+                          <input
+                            key={star}
+                            type="radio"
+                            name={`rating-${r.id}`}
+                            className="mask mask-star-2 bg-green-500"
+                            aria-label="1 star"
+                            checked={r.rating === star}
+                            readOnly
+                          />
+                        ))}
+                      </div>
                     </div>
                   </div>
-                  <br />
-                  <div className="avatar flex justify-center mb-3">
-                    <div className="w-24 rounded-full">
-                      <img
-                        src={
-                          r.user.image
-                            ? `https://res.cloudinary.com/tanvirulislam149/${r.user.image}`
-                            : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-                        }
-                      />
-                    </div>
-                  </div>
-                  <p className="text-center font-bold">{r.user.email}</p>
                   <FaQuoteLeft />
                   <p>{r.comment}</p>
                   <p>
