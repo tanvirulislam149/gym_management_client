6b2c3a2d9b915d834b2edb42d351decd458afc0d
tanvir <tanvirulislam149@gmail.com>
2025-12-05 21:53:14 +0600

    class edited

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ children }) => {
   return (
     <div>
-      <button className="bg-gradient-to-r from-green-500 to-emerald-600 hover:from-green-600 hover:to-emerald-700 text-black font-semibold py-3 px-8 rounded-lg text-lg transition-all duration-300 transform hover:scale-105">
+      <button className="bg-gradient-to-r from-green-500 to-emerald-600 hover:from-green-600 hover:to-emerald-700 text-black font-semibold py-2 px-6 rounded-lg text-lg transition-all duration-300 transform hover:scale-105 cursor-pointer">
         {children}
       </button>
     </div>
