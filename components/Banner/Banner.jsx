35562f42dd96ac20a93d9fa25728e1a588ee1cad
tanvir <tanvirulislam149@gmail.com>
2025-12-18 12:55:50 +0600

    chat icon added

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -77,6 +77,9 @@ const Banner = () => {
           </div>
         </div>
       </div>
+      <div className="text-base fixed md:right-10 right-2 md:bottom-5 bottom-2 z-50 cursor-pointer">
+        <MessageCont />
+      </div>
     </section>
   );
 };
