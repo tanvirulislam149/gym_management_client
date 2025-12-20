2debc792f939adfbbf9bd992f26a4623000aa2bf
tanvir <tanvirulislam149@gmail.com>
2025-12-20 22:04:35 +0600

    class ssr done

--- components/Fitness_classes/Fitness_classes.jsx
+++ components/Fitness_classes/Fitness_classes.jsx
@@ -1,77 +1,22 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "../Button/Button";
-import gym from "../../images/gym.jpg";
 import Image from "next/image";
 import Link from "next/link";
-import axios from "axios";
 
-const Fitness_classes = () => {
-  const [classes, setClasses] = useState([]);
-
-  const [loading, setLoading] = useState(false);
-  console.log(classes);
-
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get("https://gym-management-henna.vercel.app/fitness_classes/")
-      .then((res) => setClasses(res.data))
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
-  }, []);
-
-  // let classes = [];
-  // try {
-  //   const data = await fetch(
-  //     "https://gym-management-henna.vercel.app/fitness_classes/",
-  //     {
-  //       cache: "no-store",
-  //     }
-  //   );
-  //   classes = await data.json();
-  // } catch (error) {
-  //   console.log(error);
-  // }
+const Fitness_classes = async () => {
+  let classes = [];
+  try {
+    const data = await fetch(
+      "https://gym-management-henna.vercel.app/fitness_classes/",
+      {
+        cache: "no-store",
+      }
+    );
+    classes = await data.json();
+  } catch (error) {
+    console.log(error);
+  }
   return (
-    // <div id="classes" className="max-w-[1200px] mx-auto my-20">
-    //   <p className="text-primary font-bold text-3xl lg:text-5xl ml-10 mb-10">
-    //     -Fitness Classes
-    //   </p>
-    //   {loading ? (
-    //     <div className="flex justify-center h-50">
-    //       <span className="loading loading-spinner loading-xl"></span>
-    //     </div>
-    //   ) : (
-    //     <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
-    //       {classes?.map((c) => (
-    //         <div
-    //           key={c.id}
-    //           className="card mx-5 lg:mx-0 shadow-sm h-100 bg-base-200"
-    //         >
-    //           <figure className="">
-    //             <Image
-    //               className="h-40"
-    //               src={c.image}
-    //               width={500}
-    //               height={500}
-    //               alt="Picture of the author"
-    //             />
-    //           </figure>
-    //           <div className="card-body">
-    //             <h2 className="card-title">{c.name}</h2>
-    //             <p>{c.description.slice(0, 100)}...</p>
-    //             <div className="card-actions justify-end">
-    //               <Button>
-    //                 <Link href={`/classes/${c.id}`}>Join now</Link>
-    //               </Button>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   )}
-    // </div>
     <section id="classes" className="py-16">
       <div className="container mx-auto px-4">
         <div className="text-center mb-16">
